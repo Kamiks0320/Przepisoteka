8ea2117e7958bc5c9915ad714218beea0de33d15
Kamil <wegrzynk322@gmail.com>
2026-01-11 14:06:32 +0100

    Home page changes - added an all ingredients fetch, and a header with a navigation bar

---
+++ przepisy-front/src/app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <header className="h-14 border-b">
+            <nav className="max-w-6xl mx-auto flex items-center justify-between px-6 h-full">
+                <Link href="/" className="text-lg-font-semibold">Przepisoteka</Link>
+
+                <div className="flex gap-4">
+                    <Link href="/" className="hover:underline">Zaloguj się</Link>
+                    <Link href="/" className="hover:underline">Utwórz konto</Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
--- przepisy-front/src/app/page.tsx
+++ przepisy-front/src/app/page.tsx
@@ -1,28 +1,48 @@
 import Link from "next/link";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:5220/api/recipe", {cache: "no-store"})
+  const [recipeRes, ingredientRes] = await Promise.all([
+    fetch("http://localhost:5220/api/recipe", {cache: "no-store"}),
+    fetch("http://localhost:5220/api/ingredient", {cache: "no-store"})
+  ]);
 
-  if(!res.ok) return <p>Błąd ładowania</p>
+  if(!recipeRes.ok || !ingredientRes.ok) return <p>Błąd ładowania</p>
 
-  const recipes = await res.json();
+  const recipes = await recipeRes.json();
+  const ingredients = await ingredientRes.json();
 
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Przepisy kulinarne:</h1>
+    <main className="p-5 grid grid-cols-2 gap-5">
+      <section>
+        <h1 className="text-2xl font-bold mb-4">Przepisy kulinarne:</h1>
 
-      <ul className="space-y-2">
-        {recipes.map((recipe: any) => (
-          <li key={recipe.id} className="border p-4 rounded">
-            <h2 className="text-lg font-semibold">
-              {recipe.name}
-            </h2>
-            <Link href={`/recipes/${recipe.id}`} className="">Szczegóły</Link>
-          </li>
-        ))}
-      </ul>
+        <ul className="space-y-2">
+          {recipes.map((recipe: any) => (
+            <li key={recipe.id} className="border p-4 rounded">
+              <h2 className="text-lg font-semibold">
+                {recipe.name}
+              </h2>
+              <Link href={`/recipes/${recipe.id}`} className="">Szczegóły</Link>
+            </li>
+          ))}
+        </ul>
 
-      <Link href={`/recipes/add`}>Dodaj przepis</Link>
+        <Link href={`/recipes/add`}>Dodaj przepis</Link>
+      </section>
+
+      <section>
+        <h1 className="text-2xl font-bold mb-4">Składniki w bazie danych:</h1>
+
+        <ul className="space-y-1">
+          {ingredients.map((ingredient: any) => (
+            <li key={ingredient.id} className="border p-1 rounded">
+              <h2 className="text-lg font-semibold">
+                {ingredient.name}
+              </h2>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
